fa00e02c86a82ceb031a1ff691890abf1e2713d3
centonhuang <centonhuang@tencent.com>
2024-05-12 14:38:59 +0800

    🐞 fix(scripts): tiny fix

--- scripts/autoawq.sh
+++ scripts/autoawq.sh
@@ -1,4 +1,4 @@
-python ./scripts/quantize/autoawq.py \
+python ./scripts/autoawq.py \
 --model_path ../../model/Qwen1.5-4B-Chat \
 --quant_path ../../model/Qwen1.5-4B-gptq-4bit-Chat \
 --dataset_path ../data/aris_alpaca_deepseek_34k_zh.json \
--- scripts/autogptq.sh
+++ scripts/autogptq.sh
@@ -1,4 +1,4 @@
-python ./scripts/quantize/autogptq.py \
+python ./scripts/autogptq.py \
 --model_path ../../model/Qwen1.5-4B-Chat \
 --quant_path ../../model/Qwen1.5-4B-gptq-4bit-Chat \
 --alpaca_dataset ../../dataset/alpaca_zh/alpaca_gpt4_data_zh.json \
