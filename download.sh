491ac0b1cbdb0bc7ad343ea933cbe3e973420829
centonhuang <centonhuang@tencent.com>
2024-05-16 22:31:56 +0800

    ✨ feat(script): update download scripts

---
+++ download.sh
@@ -0,0 +1,4 @@
+HF_ENDPOINT=https://hf-mirror.com huggingface-cli download \
+    --resume-download Aris-AI/Aris-Qwen1.5-4B-Chat-Agent-8K-20240510 \
+    --local-dir Aris-Qwen1.5-4B-Chat-Agent-8K-20240510 \
+    --local-dir-use-symlinks False
