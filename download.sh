6cd1306550b20c38017b493c90d46ecfdf390fb5
centonhuang <centonhuang@tencent.com>
2024-06-01 01:41:23 +0800

    ✨ feat(script): use hf_tranfer

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
-HF_ENDPOINT=https://hf-mirror.com huggingface-cli download \
-    --resume-download Aris-AI/Aris-Qwen1.5-4B-Chat-Agent-8K-20240510 \
-    --local-dir Aris-Qwen1.5-4B-Chat-Agent-8K-20240510 \
+HF_HUB_ENABLE_HF_TRANSFER=1 HF_ENDPOINT=https://hf-mirror.com huggingface-cli download \
+    --resume-download Aris-AI/Aris-Qwen1.5-14B-Chat-Agent-16K-LoRA-20240527 \
+    --local-dir Aris-Qwen1.5-14B-Chat-Agent-16K-LoRA-20240527 \
     --local-dir-use-symlinks False
